4892772ce41b9f46e969b4309ed9525a0e1029ac
Washington Crisóstomo <washington.parra@gmail.com>
2021-09-23 08:06:09 -0300

    Created InstituicaoController

---
+++ Capitulo01/Controllers/InstituicaoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capitulo01.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

c814672c57988870563fa87c22d4fe77285f72d1
Washington Crisóstomo <washington.parra@gmail.com>
2021-09-23 08:14:51 -0300

    Created Instituicao Model

---
+++ Capitulo01/Models/Instituicao.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capitulo01.Models
+{
+    public class Instituicao
+    {
+    }
+}

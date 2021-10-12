4b37784440bcf32d9a5b6c2b995e2747157bc332
washi <washington.parra@gmail.com>
2021-10-12 07:06:21 -0300

    Action Edit

--- Capitulo01/Controllers/InstituicaoController.cs
+++ Capitulo01/Controllers/InstituicaoController.cs
@@ -45,7 +45,7 @@ namespace Capitulo01.Controllers
         };
         public IActionResult Index()
         {
-            return View(instituicoes);
+            return View(instituicoes.OrderBy(i=>i.Nome));
         }
 
         // GET: Create
@@ -60,6 +60,20 @@ namespace Capitulo01.Controllers
         {
             instituicoes.Add(instituicao);
             instituicao.InstituicaoID = instituicoes.Select(i => i.InstituicaoID).Max() + 1;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(long id)
+        {
+            return View(instituicoes.Where(i => i.InstituicaoID == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Instituicao instituicao)
+        {
+            instituicoes.Remove(instituicoes.Where(i => i.InstituicaoID == instituicao.InstituicaoID).First());
+            instituicoes.Add(instituicao);
             return RedirectToAction("Index");
         }
     }

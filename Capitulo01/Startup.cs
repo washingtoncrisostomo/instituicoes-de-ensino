1644acedb4762dd6107ff23093ef4bf9ee445897
washi <washington.parra@gmail.com>
2021-10-13 15:52:22 -0300

    Add bootstrap

--- Capitulo01/Startup.cs
+++ Capitulo01/Startup.cs
@@ -50,7 +50,7 @@ namespace Capitulo01
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Instituicao}/{action=Index}/{id?}");
             });
         }
     }
